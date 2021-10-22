4885d85854e95c5aab8540545f23668fb7a04897
Edgar Benavides <edgarben27@gmail.com>
2021-10-22 14:48:35 -0500

    Last update

--- app/src/main/java/com/example/basededatoslocalconroom/MainActivity.java
+++ app/src/main/java/com/example/basededatoslocalconroom/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.room.Room;
 
 import android.app.Application;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
@@ -16,7 +17,7 @@ import com.example.basededatoslocalconroom.data.UserDao;
 public class MainActivity extends AppCompatActivity {
 
     AppDatabase db;
-    Button btnInsert;
+    Button btnInsert, btnRead;
     AsyncTask<Integer,Integer,Integer> tareaAsincrona = new AsyncTask<Integer, Integer, Integer>(){
         @Override
         protected Integer doInBackground(Integer... integers) {
@@ -35,8 +36,11 @@ public class MainActivity extends AppCompatActivity {
         tareaAsincrona.execute();
 
         btnInsert = findViewById(R.id.btnInsert);
+        btnRead = findViewById(R.id.btnRead);
 
         btnInsert.setOnClickListener(view -> {
+
+            AppDatabase db = AppDatabase.getDatabaseInstance(getApplication());
             UserDao dao = db.userDao();
 
             AppDatabase.databaseWriteExecutor.execute(() -> {
@@ -46,8 +50,27 @@ public class MainActivity extends AppCompatActivity {
                 dao.insertAll(user);
             });
 
-            for(User u : dao.getAll()){
-                Log.d("DBUsuario", u.firstName + " " + u.lastName);
+            Log.d("DBUsuario", "Elemento insertado");
+
+        });
+
+        btnRead.setOnClickListener(view -> {
+
+            AppDatabase db = AppDatabase.getDatabaseInstance(getApplication());
+            UserDao dao = db.userDao();
+
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.N){
+                AppDatabase.databaseWriteExecutor.execute(() -> {
+                    dao.getAll().stream().forEach(user -> {
+                        Log.i("Consulta User", user.uid + " " + user.firstName + " " + user.lastName);
+                    });
+                });
+            }else{
+                AppDatabase.databaseWriteExecutor.execute(() -> {
+                    for (User user : dao.getAll()){
+                        Log.d("DBUsuario", user.firstName + " " + user.lastName);
+                    }
+                });
             }
 
         });
